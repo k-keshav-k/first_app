c87d3110d72c426dff733269b2497ef98d6029b8
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-01-12 13:30:19 +0530

    updated

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div className="modal">
+      <p>Are you sure ?</p>
+      <buton className="btn btn--alt">Cancel</buton>
+      <button className="btn">Confirm</button>
+    </div>
+  );
+}
+
+export default Modal;
